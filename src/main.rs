bf48587becd19dacd6fcd775ed1ac4774cacd73f
Woonters <woonters@gmail.com>
2023-12-20 22:43:31 +0000

    very starting sections of a bot
    lots to do but this is coming along quickly

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,71 @@
 use reqwest::get;
+use serenity::async_trait;
+use serenity::framework::standard::{
+    macros::{command, group},
+    CommandResult, Configuration, StandardFramework,
+};
+use serenity::model::channel::Message;
+use serenity::prelude::*;
 use std::{
+    env,
     fs::File,
     io::{stdin, Write},
 };
-use tokio;
 use urlencoding::encode;
 
-#[tokio::test]
-async fn test_voice() {
-    let body = get("https://api.novelai.net/ai/generate-voice?text=HELLO%20SIR%21%21%21&seed=aHaleAndHeartySir&voice=-1&opus=false&version=v2");
-    match body.await {
-        Ok(resp) => {
-            if resp.status().is_success() {
-                let bytes = resp.bytes().await.unwrap();
-                println!("Bytes: {:?}", bytes);
-                let mut file = File::create("temp.mpeg").expect("I HAVE FAILED TO CREATE THE FILE");
-                file.write_all(&bytes).unwrap();
-                println!("HELLO SIR I HAVE GOT A MESSAGE FOR YOU");
-            } else {
-                println!("Oh no you have failed the challenge: {}", resp.status())
-            }
-        }
-        Err(e) => {
-            println!("Request failed: {}", e)
-        }
+#[group]
+#[commands(ping, say)]
+struct General;
+
+struct Handler;
+
+#[async_trait]
+impl EventHandler for Handler {}
+
+#[tokio::main]
+async fn main() {
+    let framework = StandardFramework::new().group(&GENERAL_GROUP);
+    framework.configure(Configuration::new().prefix("~"));
+    // that's our prefix, we look for messages with that
+
+    let token = env::var("DISCORD_TOKEN").expect("Token error");
+    let intents = GatewayIntents::non_privileged() | GatewayIntents::MESSAGE_CONTENT;
+    let mut client = Client::builder(token, intents)
+        .event_handler(Handler)
+        .framework(framework)
+        .await
+        .expect("Failed creating discord client");
+    if let Err(why) = client.start().await {
+        println!(
+            "An Error {} has occurred whilst starting discord client",
+            why
+        );
     }
+}
+
+#[command]
+async fn ping(ctx: &Context, msg: &Message) -> CommandResult {
+    tokio::task::spawn_blocking(|| {
+        let text = fix_input(&get_input());
+        get_voice_and_save(&text);
+    })
+    .await
+    .expect("Task Panicked");
+    msg.reply(ctx, format!("Pong - {}", msg.content)).await?;
+    Ok(())
+}
+
+#[command]
+async fn say(ctx: &Context, msg: &Message) -> CommandResult {
+    let content = msg.content.clone();
+    tokio::task::spawn_blocking(move || {
+        let text = fix_input(&content[5..]);
+        get_voice_and_save(&text);
+    })
+    .await
+    .expect("Task Panicked");
+    msg.reply(ctx, "I Have just created the file;").await?;
+    Ok(())
 }
 
 #[tokio::main]
@@ -54,16 +95,22 @@ fn get_input() -> String {
     stdin()
         .read_line(&mut input)
         .expect("I DID NOT UNDERSTAND THAT SIR");
-    return input;
+    input
 }
 
 fn fix_input(input: &str) -> String {
     let binding = input.to_uppercase();
     let encoded = encode(&binding);
-    return encoded.into_owned();
+    encoded.into_owned()
 }
+mod tests {
+    // Tests are atm not working, I don't think that's the worst thing in the world atm, but I
+    // would like to get it working eventually
+    use crate::{fix_input, get_input, get_voice_and_save};
 
-fn main() {
-    let to_say = fix_input(&get_input());
-    get_voice_and_save(&to_say);
+    #[test]
+    fn test_voice_api() {
+        let text = fix_input(&get_input());
+        get_voice_and_save(&text);
+    }
 }
